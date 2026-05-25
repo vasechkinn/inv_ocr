f12c1cf4288f0276c37e362f156f162e7c0c59b6
vasechkin <vasechkin@gmail.com>
2026-05-26 00:18:32 +0300

    feat: version 2. -> 3.

--- ocr_services/ocr_model.py
+++ ocr_services/ocr_model.py
@@ -1,4 +1,5 @@
 import os
+os.environ["FLAGS_use_mkldnn"] = "0"
 import logging
 from paddleocr import PaddleOCR
 from dataclasses import dataclass, field
@@ -7,37 +8,40 @@ from typing import Optional
 logging.getLogger("ppocr").setLevel(logging.ERROR)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_DIR = os.path.join(BASE_DIR, "models")
-
 
 @dataclass(frozen=True)
 class OCRConfig:
     lang: str = 'ru'
-    model_dir: str = MODEL_DIR
     use_doc_orientation_classify: bool = True
-    use_textline_orientation: bool = True
+    use_doc_unwarping: bool = False
+    use_textline_orientation: bool = False
     use_gpu: bool = False
-    show_log: bool = False
+    # show_log: bool = False
 
 class OCREngine:
     _example: Optional['OCREngine'] = None
 
-    def __new__(cls, config: Optional['OCRConfig'] = None):
+    def __new__(cls, config: Optional[OCRConfig] = None):
         if cls._example is None:
             print("инициализация паддлы...")
             cls._example = super().__new__(cls)
 
             if config is None:
                 config = OCRConfig()
+            
+            device = 'gpu' if config.use_gpu else 'cpu'
+            # log_level = logging.ERROR if not config.show_log else logging.INFO
 
-            cls._example.ocr = PaddleOCR(
-                lang = config.lang,
-                model_dir = config.model_dir,
-                use_doc_orientation_classify = config.use_doc_orientation_classify,
-                use_textline_orientation = config.use_textline_orientation,
-                use_gpu = config.use_gpu,
-                show_log = config.show_log,
-            )
+            params = {
+                'lang': config.lang,
+                'use_doc_orientation_classify': config.use_doc_orientation_classify,
+                'use_doc_unwarping': config.use_doc_unwarping,
+                'use_textline_orientation': config.use_textline_orientation,
+                'device': device,
+                # 'log_level': log_level,
+                'enable_mkldnn': False,
+            }
+            cls._example.ocr = PaddleOCR(**params)
         return cls._example
 
     def get_ocr(self):
@@ -46,11 +50,11 @@ class OCREngine:
         """
         return self.ocr
 
-_ocr: Optional[PaddleOCR] = None
+_ocr_model: Optional[PaddleOCR] = None
 
 def load_ocr(config: Optional[OCRConfig] = None):
-    global _ocr
-    if _ocr is None:
+    global _ocr_model
+    if _ocr_model is None:
         model = OCREngine(config)
-        _ocr = model.get_ocr()
-    return _ocr
+        _ocr_model = model.get_ocr()
+    return _ocr_model
