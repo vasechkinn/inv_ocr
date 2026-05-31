af92842d3222e71d34526273647cb7924821ca88
vasechkin <vasechkin@gmail.com>
2026-06-01 00:18:22 +0300

    fix: вывод всех параметров(

--- test.py
+++ test.py
@@ -2,6 +2,12 @@ import sys
 import os
 from ocr_services.ocr_model import load_ocr
 from ocr_services.utils import save_byte_files, del_temp_files
+from regs import (
+    get_num_account, get_data, get_summa,
+    get_nds_procent, get_nds_sum,
+    get_provider_inn, get_provider_num_account, get_provider_name,
+    get_buyer_inn, get_fio_buyer, get_buyer_name
+)
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -34,9 +40,21 @@ def test_img(img: str):
                 texts = []
         full_text = "\n".join(texts) if isinstance(texts, list) else str(texts)
         print(full_text)
+        print(f"Номера счетов (20 цифр): {get_num_account(full_text)}")
+        print(f"Дата: {get_data(full_text)}")
+        print(f"Сумма: {get_summa(full_text)}")
+        print(f"НДС %: {get_nds_procent(full_text)}")
+        print(f"Сумма НДС: {get_nds_sum(full_text)}")
+        print(f"ИНН поставщика: {get_provider_inn(full_text)}")
+        print(f"Р/с поставщика: {get_provider_num_account(full_text)}")
+        print(f"Название поставщика: {get_provider_name(full_text)}")
+        print(f"ИНН покупателя: {get_buyer_inn(full_text)}")
+        print(f"ФИО покупателя: {get_fio_buyer(full_text)}")
+        print(f"Название покупателя: {get_buyer_name(full_text)}")
     else:
         print("текст не найден.")
 
 
 if __name__ == "__main__":
-    test_img("работа с текстом.png")
+    test_img("платежка.jpg")
+    
