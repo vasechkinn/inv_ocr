577958be3633ec04d7768b4f3945fa4a96c1251d
vasechkinn <vasechinnnn@gmail.com>
2026-06-08 00:27:36 +0300

    info: rename func name

--- ocr_services/warm_ocr.py
+++ ocr_services/warm_ocr.py
@@ -1,9 +1,11 @@
 from ocr_services.ocr_model import load_ocr
 
-ocr_model  = None
-def init_oct():
+ocr_model = None
+
+
+def init_ocr():
     global ocr_model
     if ocr_model is None:
         ocr_model = load_ocr()
-        print('паддла загружена (▽◕ ᴥ ◕▽)')
+        print("паддла загружена (▽◕ ᴥ ◕▽)")
     return ocr_model
