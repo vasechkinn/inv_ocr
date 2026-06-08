41371271f22cddd9b403dc1c310029d15c06f382
vasechkinn <vasechinnnn@gmail.com>
2026-06-09 01:24:23 +0300

    feat: create all

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from database import engine, Base
 from web_app.routers import upload, save
 from ocr_services.warm_ocr import init_ocr
+from fastapi.responses import FileResponse
+
+
+Base.metadata.create_all(bind=engine)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,12 +16,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="OCR Invoice Parser", lifespan=lifespan)
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-# templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="web_app/static"), name="static")
 
 app.include_router(upload.router)
 app.include_router(save.router)
 
-# @app.get("/")
-# async def index(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+async def index():
+    return FileResponse("web_app/static/index.html")
