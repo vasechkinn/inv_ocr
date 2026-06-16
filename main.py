34f591689c4a556b24d241cfa0b65ebb4779cb6d
vasechkinn <vasechinnnn@gmail.com>
2026-06-16 23:03:29 +0300

    feat: new rout

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from database import engine, Base
-from web_app.routers import upload, save
+from web_app.routers import upload, save, auth
 from ocr_services.warm_ocr import init_ocr
 from fastapi.responses import FileResponse
 
@@ -20,6 +20,7 @@ app = FastAPI(title="OCR Invoice Parser", lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="web_app/static"), name="static")
 
+app.include_router(auth.router, prefix="/api/auth")
 app.include_router(upload.router)
 app.include_router(save.router)
 
