2ff761f664b76e48e000005f863a8bbaff0c7cfd
vasechkinn <vasechinnnn@gmail.com>
2026-06-16 22:59:36 +0300

    feat: MYsql

--- database.py
+++ database.py
@@ -1,17 +1,34 @@
-from sqlalchemy import (
-    create_engine,
+import os
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+print("DB_PASSWORD raw:", repr(DB_PASSWORD))
+
+if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
+    raise ValueError("Не все переменные окружения для БД установлены")
+
+encoded_password = quote_plus(DB_PASSWORD)
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{encoded_password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
 
 
 class Base(DeclarativeBase):
     pass
-
-
-engine = create_engine(
-    "sqlite:///database.db", echo=True, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(bind=engine)
 
 
 def get_db():
