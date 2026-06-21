7f2e421b932a18ec18ccab3b19534aac6ca135de
vasechkinn <vasechinnnn@gmail.com>
2026-06-21 23:28:32 +0300

    feat: sql_lite + mysql

--- database.py
+++ database.py
@@ -1,29 +1,45 @@
 import os
-from dotenv import load_dotenv
 from urllib.parse import quote_plus
 
-load_dotenv()
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+load_dotenv()
 
-print("DB_PASSWORD raw:", repr(DB_PASSWORD))
+DB_TYPE = os.getenv("DB_TYPE", "mysql")
 
-if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
-    raise ValueError("Не все переменные окружения для БД установлены")
+if DB_TYPE == "sqlite":
+    DB_NAME = os.getenv("DB_NAME", "inv_ocr.db")
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///./{DB_NAME}"
 
-encoded_password = quote_plus(DB_PASSWORD)
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{encoded_password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        echo=True,
+        connect_args={"check_same_thread": False},
+    )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+elif DB_TYPE == "mysql":
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+
+    if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
+        raise ValueError("Не все переменные окружения для MySQL установлены")
+
+    encoded_password = quote_plus(DB_PASSWORD)
+    SQLALCHEMY_DATABASE_URL = (
+        f"mysql+pymysql://{DB_USER}:{encoded_password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+else:
+    raise ValueError(f"Неподдерживаемый DB_TYPE: {DB_TYPE}")
+
+
 SessionLocal = sessionmaker(bind=engine)
 
 
